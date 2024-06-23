71d6532999f1e67c91fe8ae212b147b86a60abba
rajeebkm <rkm2541997@gmail.com>
2024-06-23 16:55:44 +0530

    address updated

--- frontend/src/app/utils/constant.ts
+++ frontend/src/app/utils/constant.ts
@@ -3,9 +3,7 @@ export const themeConstant = {
     LIGHT: 'light'
 }
 
-export const ETH_SEPOLIA:string = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
-export const STRK_SEPOLIA:string = "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
-
-export const SCHEMA_REGISTRY:string = "0x79bf4b90f4447d4abc7a1b6d6f6482090ed3811b63bedb3efad3fe911cb9f5c"
-
-export const SAS:string ="0x12b9a64d418c069178961307941089504e7cb399807fd05d0a96bf624a28d81";
+export const ETH_SEPOLIA: string = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+export const STRK_SEPOLIA: string = "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
+export const SCHEMA_REGISTRY: string = "0x6c6fd3dae5772a33cce7952d339242ea6d66b8f48f6b37f5091a2bc1369ab60"
+export const SAS: string = "0x25ee1ccc00d7239a99ae2103fd9692b93e05e75fa801938a025cbe5be2fbefa";
